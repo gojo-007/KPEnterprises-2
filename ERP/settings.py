d675ae498569b0eabd5ee6a0d5265445b4b4ba60
Gojo Saturo <142395257+gojo-007@users.noreply.github.com>
2024-07-18 14:41:47 +0530

    Update settings.py

--- ERP/settings.py
+++ ERP/settings.py
@@ -139,7 +139,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
